509edff5a0805c63e946c4dfdc36b3cdfba08498
Justin Kissling <st107873@stud.uni-stuttgart.de>
2018-11-27 19:47:56 +0100

    Removed commented out code at line 10

--- Calculator/src/main/java/calculator/Calculator.java
+++ Calculator/src/main/java/calculator/Calculator.java
@@ -1,26 +1,25 @@
 package calculator;
 
-public class Calculator extends SuperCalculator {
-	// Test Fork
-    private String resultAsText;
+public class Calculator extends SuperCalculator {    
 
-    public final static int MULTIPLIER = 10;
+private String resultAsText;
 
-    public double addition(double a, double b) {
-        // int i = a + b;
-        return a + b;
-    }
+public static final int MULTIPLIER = 10;
 
-    public double multiplikation(double a, double b, double c) {
-        return a * b;
-    }
+public double addition(double a, double b) {
+    return a + b;
+}
 
-    public double subtraction(double a, double b) {
-        return a - b;
-    }
+public double multiplikation(double a, double b, double c) {
+    return a * b;
+}
 
-    public double division(double a, double b) {
-        return a / b;
-    }
+public double subtraction(double a, double b) {
+    return a - b;
+}
+
+public double division(double a, double b) {
+    return a / b;
+}
 }
 
