6381a15f1321345555696c9cfba4c08362d42d10
Justin Kissling <st107873@stud.uni-stuttgart.de>
2018-12-03 12:24:57 +0100

    More commented out code

--- Calculator/src/main/java/calculator/Calculator.java
+++ Calculator/src/main/java/calculator/Calculator.java
@@ -4,18 +4,29 @@ public class Calculator extends SuperCalculator {
 	// Test Fork
     private String resultAsText;
 
-    public final static int MULTIPLIER = 10;
+    public static final int MULTIPLIER = 10;
 
+    @Override
     public double addition(double a, double b) {
         // int i = a + b;
+        // int j = a - b;
         return a + b;
     }
 
     public double multiplikation(double a, double b, double c) {
+        /*
+        int i = a + b;
+        Blubb
+        */
         return a * b;
     }
 
     public double subtraction(double a, double b) {
+        /**
+         * int i = a + b;
+         * 
+         * Und hier ein richtiger Kommentar!
+         */
         return a - b;
     }
 
