93813f1dac09dc59925206d7d27585d762102fd8
Justin Kissling <st107873@stud.uni-stuttgart.de>
2018-12-03 13:04:30 +0100

    Removed commented out code at line 11

---
+++ Calculator/src/main/java/calculator/temp.java
@@ -0,0 +1,36 @@
+package calculator;
+
+public class Calculator extends SuperCalculator {
+	// Test Fork
+    private String resultAsText;
+
+    public static final int MULTIPLIER = 10;
+
+    @Override
+    public double addition(double a, double b) {
+        // int j = a - b;
+        return a + b;
+    }
+
+    public double multiplikation(double a, double b, double c) {
+        /*
+        int i = a + b;
+        Blubb
+        */
+        return a * b;
+    }
+
+    public double subtraction(double a, double b) {
+        /**
+         * int i = a + b;
+         * 
+         * Und hier ein richtiger Kommentar!
+         */
+        return a - b;
+    }
+
+    public double division(double a, double b) {
+        return a / b;
+    }
+}
+
